82aac3ef6ba72ac6ba488a40e47e1c230f926506
cocobabb <cocobab05108@gmail.com>
2025-06-06 16:49:54 +0900

    [feat] 폰트 적용 및 반응형 설정

--- animation/src/App.jsx
+++ animation/src/App.jsx
@@ -1,67 +1,34 @@
-import { motion } from "framer-motion";
+import { clamp, motion } from "framer-motion";
 
 export default function App() {
   return (
     <section>
-      <div>App</div>
-      <img src="/images/peach.jpg" alt="" />
-      <img src="/images/peach.jpg" alt="" />
-      <img src="/images/peach.jpg" alt="" />
+      {/* CSS의 clamp함수
+        clamp(최소값, 유동값, 최대값)
+        최소값: 요소의 제일 작은 크기 설정
+        유동값: viewPort 너비의 설정% 크기 만큼 증가 
+          ex) 2vw => 제일 작거나 큰 화면이 아닌 화면 크기이면 화면 너비의 2% 크기 만큼 증감
+        최대값: 요소의 제일 큰 크기 설정
 
-      <motion.img
-        src="/images/painting_waterful_peach.jpg"
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
-        transition={{ duration: 2, ease: "easeOut" }}
-        viewport={{ once: true }} // 모션 한번만(설정 안하면 viewPort에 잡힐 때마다 모션 실행됨)
+        최소값, 최대값은 rem, px, vw 등으로 조절
+      */}
+      <motion.h1 className="font-nanum text-[clamp(1rem,8vw,5rem)] font-bold text-pink-400">
+        복숭아
+      </motion.h1>
+
+      {/* Tailwind에서 clamp 적용*/}
+      <img
+        src="/images/peach.jpg"
+        alt="복숭아"
+        className="w-[clamp(5vw,30vw,100vw)]"
       />
 
-      <motion.img
+      {/* 일반적인 CSS환경에서 clamp적용 */}
+      <img
         src="/images/peach.jpg"
-        whileInView={{ x: 100 }}
-        transition={{
-          type: "spring",
-          stiffness: 200, // 크기가 클수록 강하게 당김
-          damping: 1, // 마찰의 크기, 크기가 클수록 튕김이 없음
-        }}
-      />
-
-      <motion.img
-        src="/images/peach.jpg"
-        drag // 해당 태그 드래그 가능하게 함
-        dragConstraints={{ left: -100, right: 400 }} // 드래그 가능 범위 설정
-        // 관성 효과처럼 부드럽게 멈추게 함
-        dragTransition={{
-          bounceStiffness: 600, // 튕기는 강도
-          bounceDamping: 30, // 마찰 크기
-        }}
-      />
-
-      <motion.img
-        src="/images/peach.jpg"
-        whileInView={{ opacity: 1 }}
-        initial={{ opacity: 0 }}
-        transition={{
-          type: "tween",
-          duration: 3,
-          ease: "easeInOut",
-          // repeat: Infinity, // 모션 반복 횟수
-          // repeatDelay: 1, // 모션 재실행 시간
-        }}
-      />
-      <motion.img
-        src="/images/peach.jpg"
-        whileInView={{
-          scale: [1, 2, 2, 1, 1],
-          rotate: [0, 0, 180, 180, 0],
-          borderRadius: ["0%", "0%", "50%", "50%", "0%"],
-        }}
-        transition={{
-          duration: 2,
-          ease: "easeInOut",
-          times: [0, 0.2, 0.5, 0.8, 1],
-          // repeat: Infinity,
-          // repeatDelay: 1,
+        alt="복숭아"
+        style={{
+          width: "clamp(100px, 30vw, 400px)",
         }}
       />
     </section>
