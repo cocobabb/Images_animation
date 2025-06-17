9ff35c3a8f8631cdf36bb9d9c32772dd05303864
cocobabb <cocobab05108@gmail.com>
2025-06-17 20:33:07 +0900

    [refactor] 하드 코딩된 api 주소 환경변수화

--- animation/vite.config.js
+++ animation/vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": `http://localhost:5000`,
+export default defineConfig(({ mode }) => {
+  // loadEnv를 사용하여 환경변수 로드
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": env.VITE_API_SERVER,
+      },
     },
-  },
+  };
 });
